f2c9c084dce1b772a90e0d5e01f2adb0b6cf80d3
ther <winlab@thor-demeestere.be>
2023-09-15 23:58:13 +0200

    f

--- src/main.go
+++ src/main.go
@@ -19,89 +19,29 @@ var (
 
 func main() {
 
-	// Find all available network devices
-	devices, err := pcap.FindAllDevs()
+	handle, err := pcap.OpenLive(selectDevice(), 65536, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = handle.SetBPFFilter("tcp")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Print information about each network device
-	for _, device := range devices {
-		// Filter out virtual network devices
-		if !(strings.Contains(device.Description, "VMnet")) && !(strings.Contains(device.Description, "Virtual")) {
-			log.Printf("Name: %s\nDescription: %s\n", device.Name, device.Description)
-		}
-	}
-
-	// Open a network interface for capturing packets
-	handle, err := pcap.OpenLive("\\Device\\NPF_Loopback", 65536, true, pcap.BlockForever)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = handle.SetBPFFilter("tcp") // Set a BPF filter to capture only TCP packets
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a packet source from the handle
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./src/index.html") // Specify the correct path to your HTML file
-	})
+	http.HandleFunc("/", handleSummonWebpage)
+	http.HandleFunc("/updatePackets", handleUpdatePackets)
 
-	http.HandleFunc("/updatePackets", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
-
-		// Create a channel for this client
-		clientChan := make(chan gopacket.Packet)
-
-		// Register the client
-		clientMu.Lock()
-		clients = append(clients, clientChan)
-		clientMu.Unlock()
-
-		// Notify when the client's connection is closed
-		closeNotifier := w.(http.CloseNotifier).CloseNotify()
-
-		go func() {
-			<-closeNotifier
-			// Handle client disconnect here
-			removeClient(clientChan)
-		}()
-
-		for {
-			// Send updates to the client
-			select {
-			case counterValue := <-clientChan:
-				fmt.Fprintf(w, "data: %d\n\n", counterValue)
-				w.(http.Flusher).Flush()
-			case <-r.Context().Done():
-				// Handle client disconnect here
-				removeClient(clientChan)
-				return
-			}
-		}
-	})
 	go func() {
 
-		// Process incoming packets
 		for packet := range packetSource.Packets() {
-			// Process the outgoing packet here
+
 			fmt.Println(packet)
 			updateClients(packet)
 		}
 	}()
 	http.ListenAndServe(":8888", nil)
-
-	/*	for {
-			time.Sleep(1 * time.Second)
-			incrementCounter()
-			updateClients(counter)
-		}
-	*/
 
 	defer handle.Close()
 }
@@ -125,10 +65,61 @@ func removeClient(clientChan chan gopacket.Packet) {
 	defer clientMu.Unlock()
 	for i, c := range clients {
 		if c == clientChan {
-			// Remove the client from the list
+
 			clients = append(clients[:i], clients[i+1:]...)
-			close(clientChan) // Close the client's channel
+			close(clientChan)
 			break
 		}
 	}
+}
+
+func handleUpdatePackets(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	clientChan := make(chan gopacket.Packet)
+
+	clientMu.Lock()
+	clients = append(clients, clientChan)
+	clientMu.Unlock()
+
+	closeNotifier := w.(http.CloseNotifier).CloseNotify()
+
+	go func() {
+		<-closeNotifier
+
+		removeClient(clientChan)
+	}()
+
+	for {
+
+		select {
+		case counterValue := <-clientChan:
+			fmt.Fprintf(w, "data: %d\n\n", counterValue)
+			w.(http.Flusher).Flush()
+		case <-r.Context().Done():
+
+			removeClient(clientChan)
+			return
+		}
+	}
+}
+func handleSummonWebpage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./src/index.html")
+}
+func selectDevice() string {
+
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, device := range devices {
+
+		if !(strings.Contains(device.Description, "VMnet")) && !(strings.Contains(device.Description, "Virtual")) {
+			log.Printf("Name: %s\nDescription: %s\n", device.Name, device.Description)
+		}
+	}
+	return "\\Device\\NPF_Loopback"
 }
