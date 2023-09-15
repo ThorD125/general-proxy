d6980d9cf7d1d5d64debaa75da61e7f43672010b
Ther <thor.demeestere@telenet.be>
2023-09-15 16:03:57 +0200

    f

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+// PageData is a struct to hold the data for your template.
+type PageData struct {
+	Title   string
+	Heading string
+	Content string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Define your HTML template.
+		const htmlTemplate = `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>{{.Title}}</title>
+		</head>
+		<body>
+			<h1>{{.Heading}}</h1>
+			<p>{{.Content}}</p>
+		</body>
+		</html>
+		`
+
+		// Parse the template.
+		tmpl, err := template.New("mytemplate").Parse(htmlTemplate)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Create a data instance.
+		data := PageData{
+			Title:   "My Page",
+			Heading: "Welcome to My Page",
+			Content: "This is the content of my page.",
+		}
+
+		// Execute the template and write the result to the HTTP response.
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	// Start the web server.
+	http.ListenAndServe(":8888", nil)
 }
