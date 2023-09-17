814de972d40d9bfe0115a068dcd7bbda44e06c4c
Ther <thor.demeestere@telenet.be>
2023-09-17 18:34:01 +0200

    f

--- src/packetstuff.go
+++ src/packetstuff.go
@@ -14,7 +14,7 @@ type appPackets struct {
 }
 
 func showpackets(packetSource *gopacket.PacketSource) {
-	apps := []appPackets{}
+	appsPakketList := []appPackets{}
 	go func() {
 		for packet := range packetSource.Packets() {
 			if !isPaused {
@@ -61,66 +61,41 @@ func showpackets(packetSource *gopacket.PacketSource) {
 				fmt.Println("myIp: ", ipv4AddrOfInterface)
 				fmt.Println("otherIp: ", otherIp)
 				fmt.Println("appPort: ", appPort)
+				appName := getAppName(appPort)
+				fmt.Println("appName ", appName)
 
-				//ipv6Layer := packet.Layer(layers.LayerTypeIPv6)
-				//if ipv6Layer != nil {
-				//	ipv6, _ := ipv6Layer.(*layers.IPv6)
-				//	if ipv6 != nil {
-				//		if ipv6.SrcIP.String() == ipv4AddrOfInterface {
-				//			fmt.Println("sent")
-				//		} else if ipv6.DstIP.String() == ipv4AddrOfInterface {
-				//			fmt.Println("received")
-				//		}
-				//	}
-				//}
-				//ethLayer := packet.Layer(layers.LayerTypeEthernet)
-				//if ethLayer != nil {
-				//	ethPacket, _ := ethLayer.(*layers.Ethernet)
-				//	fmt.Println("Ethernet Source MAC:", ethPacket.SrcMAC)
-				//	fmt.Println("Ethernet Destination MAC:", ethPacket.DstMAC)
-				//	fmt.Println("Ethernet Ethertype:", ethPacket.EthernetType)
-				//	//fmt.Println("Ethernet Contents:", ethPacket.Contents)
-				//	//fmt.Println("Ethernet Payload:", ethPacket.Payload)
-				//}
-				//if ipv4Layer != nil {
-				//	ip, _ := ipv4Layer.(*layers.IPv4)
-				//	//---
-				//	// flag is 3 bits long
-				//	// first bit is always 0
-				//	// second bit is DF (Don't Fragment) bit
-				//	// third bit is MF (More Fragments) bit
-				//	fmt.Println("IP Flags:", ip.Flags)
-				//	//FragOffset is 13 bits long
-				//	//FragOffset is the offset of the data in the original datagram, measured in units of 8 octets (64 bits).
-				//	fmt.Println("IP FragOffset:", ip.FragOffset)
-				//	//---
-				//	fmt.Println("IP Version:", ip.Version)
-				//	fmt.Println("IP Protocol:", ip.Protocol)
-				//	fmt.Println("IP SrcIP:", ip.SrcIP)
-				//	fmt.Println("IP DstIP:", ip.DstIP)
-				//	//fmt.Println("IP Contents:", ip.Contents)
-				//	//fmt.Println("IP Payload:", ip.Payload)
-				//}
-				//if ipv6Layer != nil {
-				//	ipv6, _ := ipv6Layer.(*layers.IPv6)
-				//	fmt.Println("IPv6 Version:", ipv6.Version)
-				//	fmt.Println("IPv6 NextHeader:", ipv6.NextHeader)
-				//	fmt.Println("IPv6 SrcIP:", ipv6.SrcIP)
-				//	fmt.Println("IPv6 DstIP:", ipv6.DstIP)
-				//	//fmt.Println("IPv6 Payload:", ipv6.Payload)
-				//}
-				//if tcpLayer != nil {
-				//	tmptcp, _ := tcpLayer.(*layers.TCP)
-				//	fmt.Println("TCP SrcPort:", tmptcp.SrcPort)
-				//	fmt.Println("TCP DstPort:", tmptcp.DstPort)
-				//}
-				//if udpLayer != nil {
-				//	udpLayer, _ := udpLayer.(*layers.UDP)
-				//	fmt.Println("UDP SrcPort:", udpLayer.SrcPort)
-				//	fmt.Println("UDP DstPort:", udpLayer.DstPort)
-				//}
+				isNotInList := true
+				for _, appPakket := range appsPakketList {
+					if appPakket.Name == appName && appPakket.IP == otherIp && appPakket.Port == appPort {
+						isNotInList = false
+						break
+					}
+				}
+
+				if isNotInList {
+					appsPakketList = append(appsPakketList, appPackets{
+						Name:       appName,
+						IP:         otherIp,
+						Port:       appPort,
+						packetList: []gopacket.Packet{packet},
+					})
+				} else {
+					for _, appPakket := range appsPakketList {
+						if appPakket.Name == appName && appPakket.IP == otherIp && appPakket.Port == appPort {
+							appPakket.packetList = append(appPakket.packetList, packet)
+							break
+						}
+					}
+				}
+
+				fmt.Println("appsPakketList: ", len(appsPakketList))
 			}
 		}
 	}()
 
+}
+
+func getAppName(port int) string {
+	fmt.Println("port: ", port)
+	return "test"
 }
